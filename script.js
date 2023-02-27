04e28b849d506b69f16dd000ca4c509a5b649538
kota011 <59875539+kota011@users.noreply.github.com>
2023-02-27 16:37:58 +0100

    Skoro zavrsen k Goriva

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+var potrosnja, cena, cenaStoKm, cenaKm, velicinaR, distanca;
+
 function novaCena(){
-    var potrosnja = parseFloat(document.getElementById("potrosnja").innerText);
-    var cena = parseFloat(document.getElementById("cena").innerText);
-    var cenaStoKm = potrosnja * cena;
-    var cenaKm = cenaStoKm/100;
+    potrosnja = parseFloat(document.querySelector('#potrosnja').value);
+    cena = parseFloat(document.querySelector('#cena').value);
+    cenaStoKm = potrosnja * cena;
+    cenaKm = cenaStoKm/100;
+    //console.log(cenaKm);
+}
+
+function noviRez(){
+    velicinaR = parseFloat(document.querySelector('#rezervoar').value);
+    distanca = parseFloat(document.querySelector('#distanca').value);
+    //console.log(cenaKm);
 }
 
 function rezultat(){
-    getElementById("cenaKm").innerText = 'Cena kilometra je ' + cenaKm;
+    //ovo ce ici pod novaCena a dugme ce biti izbrisano
+    document.querySelector('#cenaKilometra').innerText = 'Cena na 100 kilometara je ' + cenaKm;
+}
+
+function rezervoar(){
+    //ovo ce ici pod noviRez a dugme ce biti izbrisano
+    let distPun = (velicinaR/ potrosnja) * 100;
+    document.querySelector('#predjenoKm').innerText = 'Sa punim rezervoarmon se moze preci ' + distPun + ' km';
+    let zeljenaCena = distanca * cenaKm;
+    document.querySelector('#zeljenaDist').innerText = 'Za zeljenu distancu je potrebno '+ distanca*potrosnja/100+ ' l ('  + zeljenaCena + 'din)';
 }
 
 function resetovanje(){
